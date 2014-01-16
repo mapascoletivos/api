49f2a5ecd739a664e1f6d5550ce6d52545559e20
Vitor George <vitor.george@gmail.com>
2014-01-16 19:35:54 -0200

    json route

--- app/controllers/layers.js
+++ app/controllers/layers.js
@@ -1,3 +1,26 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose'), 
+	Layer = mongoose.model('Feature'),
+	utils = require('../../lib/utils'),
+	extend = require('util')._extend;
+  
+/**
+ * Load
+ */
+
+exports.load = function(req, res, next, id){
+	Layer.load(id, function (err, layer) {
+		if (err) return next(err)
+			if (!layer) return next(new Error('not found'))
+			req.layer = layer
+			next()
+		})
+}
+
 /**
  * New layer
  */
