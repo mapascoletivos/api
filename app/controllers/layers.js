083fa6a315bbbae0e7108e91d27ce55d8c295d7e
Miguel Peixe <miguel@cardume.art.br>
2014-01-20 19:00:59 -0200

    improvements on angular-based app

--- app/controllers/layers.js
+++ app/controllers/layers.js
@@ -4,7 +4,7 @@
  */
 
 var mongoose = require('mongoose'), 
-	Layer = mongoose.model('Feature'),
+	Layer = mongoose.model('Layer'),
 	utils = require('../../lib/utils'),
 	extend = require('util')._extend;
   
@@ -22,6 +22,51 @@ exports.load = function(req, res, next, id){
 }
 
 /**
+ * List
+ */
+
+exports.index = function(req, res){
+  var page = (req.param('page') > 0 ? req.param('page') : 1) - 1
+  var perPage = (req.param('perPage') > 0 ? req.param('perPage') : 30);
+  var options = {
+    perPage: perPage,
+    page: page
+  }
+
+  Layer.list(options, function(err, layers) {
+    if (err) return res.render('500')
+    Layer.count().exec(function (err, count) {
+
+      if (req.params.format == "json") { 
+        res.json(layers); 
+      } else {
+        res.render('layers/index', {
+          title: 'layers',
+          layers: layers,
+          page: page + 1,
+          pages: Math.ceil(count / perPage)
+        })
+      }
+    })
+  })
+}
+
+/**
+ * Show
+ */
+
+exports.show = function(req, res){
+  if (req.params.format == "json") { 
+    res.json(req.layer); 
+  } else {
+    res.render('layers/show', {
+      title: req.layer.title,
+      layer: req.layer
+    })
+  }
+}
+
+/**
  * New layer
  */
 
@@ -29,4 +74,17 @@ exports.new = function(req, res){
   res.render('layers/new', {
     title: 'New layer'
   })
+}
+
+/**
+ * Templates
+ */
+
+exports.templates = {
+	index: function(req, res) {
+		res.render('layers');
+	},
+	show: function(req, res) {
+		res.render('layers/show');
+	}
 }
--- config/routes.js
+++ config/routes.js
@@ -29,15 +29,12 @@ var featureAuth = [auth.requiresLogin, auth.feature.hasAuthorization]
 module.exports = function (app, passport) {
 
 	app.get('/', home.index);
-	app.get('/app/home', home.app);
-	app.get('/app/explore', home.explore);
 
 	// User routes
 	app.get('/login', users.login);
 	app.get('/signup', users.signup);
 	app.get('/logout', users.logout);
 	app.post('/users', users.create)
-	app.get('/dashboard', users.dashboard);
 	app.post('/users/session',
 		passport.authenticate('local', {
 		  failureRedirect: '/login',
@@ -58,16 +55,19 @@ module.exports = function (app, passport) {
 	app.del('/features/:featureId', featureAuth, features.destroy)
 	// app.put('/features/:feaureId/add/media/:featureId', layerAuth, layers.addFeature)
 
-	app.get('/api/v1/features.:format?', features.index	);
-	app.get('/api/v1/features/:featureId.:format?', features.show );
-	app.post('/api/v1/features', auth.requiresLogin, features.create)	
+	app.get('/api/v1/features.:format?', features.index);
+	app.get('/api/v1/features/:featureId.:format?', features.show);
+	app.post('/api/v1/features', auth.requiresLogin, features.create);
 
 
 	// Layers routes
 	app.param('layerId', layers.load)
 	app.get('/app/layers/new', auth.requiresLogin, layers.new)
+
+	app.get('/api/v1/layers.:format?', layers.index);
+	app.get('/api/v1/layers/:layerId.:format?', layers.show);
 	// app.post('/layers', auth.requiresLogin, layers.create)
-	// app.get('/layers/:layerId', layers.show)
+	// app.get('/app/layer', layers.show)
 	// app.put('/layers/:layerId', articleAuth, layers.update)
 	// app.del('/layers/:layerId', articleAuth, layers.destroy)
 	// app.put('/layers/:layerId/add/feature/:featureId', layerAuth, layers.addFeature)
@@ -76,6 +76,19 @@ module.exports = function (app, passport) {
 	// app.put('/layers/:layerId/remove/media/:mediaId', layerAuth, layers.addFeature)	
 	// app.put('/layers/:layerId/associate/media/:mediaId/to/feature/:featureId', layerAuth, layers.addFeature)
 	// app.put('/layers/:layerId/dissociate/media/:mediaId/from/feature/:featureId', layerAuth, layers.addFeature)
+
+	/*
+	 * Angular templates
+	 */
+	var templatePrefix = '/template';
+
+	app.get(templatePrefix + '/home', home.app);
+	app.get(templatePrefix + '/explore', home.explore);
+	app.get(templatePrefix + '/dashboard', auth.requiresLogin, users.dashboard);
+
+	// Layers
+	app.get(templatePrefix + '/layers', layers.templates.index);
+	app.get(templatePrefix + '/layers/show', layers.templates.show);
 
 	// Partials route and rendering
 	app.get('/partials/:view/:partial', function(req, res) {
