1a21d5a3b817fa8bc992ebd61b8e0fba8e780f1a
Vitor George <vitor.george@gmail.com>
2014-01-31 08:40:46 -0600

    updated test for user login

--- test/user.js
+++ test/user.js
@@ -26,66 +26,63 @@ describe('User controller', function() {
 		});
 	});
 
-  describe('POST /users/session', function() {
+	describe('POST /users/session', function() {
+		
+		describe('with good credentials', function() {
+			var agent = request.agent(app);
+			it('should create a user session', loginUser(agent));
+		});
+		
+		describe('with bad credentials', function() {
+			var agent = request.agent(app);
+			it('should be rejected', function(done) {
+				agent
+					.post('/users/session')
+					.send({ email: 'test@dummy.com', password: 'wrong' })
+					.end(function(err,res){
+						res.should.have.status(302);
+						res.header['location'].should.include(['/login']);
+						return done();
+					});
+			});
+		});
+	});
 
-    describe('with good credentials', function() {
-      var agent = request.agent(app);
-      it('should create a user session', loginUser(agent));
-    });
-
-    describe('with bad credentials', function() {
-      var agent = request.agent(app);
-      it('should be rejected', function(done) {
-        agent
-          .post('/users/session')
-          .send({ email: 'test@dummy.com', password: 'wrong' })
-          .end(onResponse);
-
-        function onResponse(err, res) {
-          res.should.have.status(200);
-          res.redirects.should.eql(['/']);
-          res.text.should.include('Sorry, that username or password was not found');
-          return done();
-        }
-      });
-    });
-
-  });
-  describe('/signout', function() {
-    var agent = request.agent(app);
-    it('should start with signin', loginUser(agent));
-    it('should sign the user out', function(done) {
-      agent.get('/signout').end(function(err, res) {
-        res.should.have.status(200);
-        res.redirects.should.eql(['/']);
-        res.text.should.include('Who are you?');
-        return done();
-      });
-    });
-    it('should destroy the user session', function(done) {
-      agent.get('/dashboard').end(function(err, res) {
-        res.should.have.status(200);
-        res.redirects.should.eql(['/']);
-        res.text.should.include('Please log in first');
-        return done();
-      });
-    });
-  });
+	describe('/signout', function() {
+	
+		var agent = request.agent(app);
+	
+		it('should start with signin', loginUser(agent));
+		
+		it('should log the user out', function(done) {
+			agent.get('/logout').end(function(err, res) {
+				res.should.have.status(302);
+				res.header['location'].should.include(['/login']);
+				return done();
+			});
+		});
+		
+		it('should destroy the user session', function(done) {
+			agent.get('/dashboard').end(function(err, res) {
+				res.should.have.status(404);
+				return done();
+			});
+		});
+	});
 });
 
 function loginUser(agent) {
-  return function(done) {
-    agent
-      .post('/users/session')
-      .send({ email: 'foobar@example.com', password: 'foobar' })
-      .end(onResponse);
+	return function(done) {
+		agent
+		.post('/users/session')
+		.send({ email: 'foobar@example.com', password: 'foobar' })
+		.end(onResponse);
 
-    function onResponse(err, res) {
-    	// test redirect to dashboard
-		res.should.have.status(302); 
-		res.header['location'].should.include('/dashboard');
-		res.text.should.include('Dashboard');
-		return done();
-    }
-  };
+		function onResponse(err, res) {
+			// test redirect to dashboard
+			res.should.have.status(302); 
+			res.header['location'].should.not.include('/login');
+			return done();
+		}
+	}
 }
