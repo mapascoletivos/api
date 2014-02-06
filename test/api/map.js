8f008730d12d003fdcb880f10b0e49381f35c88f
Vitor George <vitor.george@gmail.com>
2014-02-05 22:20:14 -0200

    feature auth and tests for map

---
+++ test/api/map.js
@@ -0,0 +1,40 @@
+/*
+ * Module dependencies.
+ */
+
+var 
+	// async = require('async'),
+	// _ = require('underscore'),
+	request = require('supertest'),
+	should = require('should'),
+	app = require('../../server'),
+	mongoose = require('mongoose');
+	// Map = mongoose.model('Map');
+	// Layer = mongoose.model('Layer'),
+	// Feature = mongoose.model('Feature'),
+	// Content = mongoose.model('Content'),
+	// User = mongoose.model('User'),
+	// Factory = require('../../lib/factory');
+
+var
+	apiPrefix = '/api/v1';
+
+describe('Map controller ', function(){
+
+	/**
+	 * Create map
+	 */
+	// describe('POST /maps', function(){
+	// 	context('When not logged in', function(){
+	// 		it('should redirect to /login', function (done) {
+	// 			request(app)
+	// 				.post(apiPrefix + '/maps')
+	// 				.expect('Content-Type', /plain/)
+	// 				.expect(302)
+	// 				.expect('Location', '/login')
+	// 				.expect(/Moved Temporarily/)
+	// 				.end(done)
+	// 		});
+	// 	});
+	// });
+});
