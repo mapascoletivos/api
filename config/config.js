e8bf9ccce59992610aa52d40bb4b035e3e74e88b
Vitor George <vitor.george@gmail.com>
2014-02-17 14:36:41 -0300

    Typo at config.js

--- config/config.js
+++ config/config.js
@@ -69,7 +69,7 @@ module.exports = {
    },
    google: {
      clientID: process.env.GOOGLE_CLIENT_ID,
-     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
      callbackURL: "http://mapascoletivos.herokuapp.com/auth/google/callback"
    }
  }
