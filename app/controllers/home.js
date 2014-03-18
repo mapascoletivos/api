5b479ad198bb089ef45ea3156988c4f8b3149ba1
Miguel Peixe <miguel@cardume.art.br>
2014-03-18 16:35:08 -0300

    fix selected projects url

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -21,38 +21,38 @@ exports.index = function (req, res) {
 			selectedProjects: [
 				{
 					title: 'Rio dos Jogos',
-					img: 'http://alpha.mapascoletivos.com.br/uploads/images/376_1_1371165797.jpg',
-					url: 'http://alpha.mapascoletivos.com.br/maps/5318c652cb160eaf6d30103a/',
+					img: 'http://www.mapascoletivos.com.br/uploads/images/376_1_1371165797.jpg',
+					url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d30103a/',
 					description: 'Jornalismo Colaborativo: cuide do Rio!'
 				},
 				{
 					title: 'São Paulo sem frescura',
-					img: 'http://alpha.mapascoletivos.com.br/uploads/images/139_1_1328364227.jpg',
-					url: 'http://alpha.mapascoletivos.com.br/maps/5318c652cb160eaf6d300f60/',
+					img: 'http://www.mapascoletivos.com.br/uploads/images/139_1_1328364227.jpg',
+					url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d300f60/',
 					description: 'Casas, padarias, botecos, barbearias, lojinhas, qualquer tipo de estabelecimento com um charme de antigamente, que mantiveram suas peculiaridades, sem entrar na onda da "modernização".'
 				},
 				{
 					title: 'Trianon Acessível',
-					img: 'http://alpha.mapascoletivos.com.br/uploads/images/396_1_1384554991.jpg',
-					url: 'http://alpha.mapascoletivos.com.br/maps/5318c652cb160eaf6d30104e/',
+					img: 'http://www.mapascoletivos.com.br/uploads/images/396_1_1384554991.jpg',
+					url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d30104e/',
 					description: 'Observar a situação das calçadas e também em estabelecimentos comerciais no entorno, o excesso de postes nas esquinas, rampas de acesso e vagas para deficientes.'
 				},
 				{
 					title: 'Cantos BA',
 					img: 'http://farm3.staticflickr.com/2620/3977617811_ced60d34e9_o.jpg',
-					url: 'http://alpha.mapascoletivos.com.br/maps/5318c652cb160eaf6d301043/',
+					url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d301043/',
 					description: ''
 				},
 				{
 					title: 'Vídeos Ambientais',
 					img: '/img/selected-projects/53235a26cc24363c3b52cbcc.png',
-					url: 'http://alpha.mapascoletivos.com.br/layers/53235a26cc24363c3b52cbcc/',
+					url: 'http://www.mapascoletivos.com.br/layers/53235a26cc24363c3b52cbcc/',
 					description: ''
 				},
 				{
 					title: 'Centros de adoção de animais',
-					img: 'http://alpha.mapascoletivos.com.br/uploads/images/300_1_1350313222.jpg',
-					url: 'http://alpha.mapascoletivos.com.br/maps/5318c652cb160eaf6d300ff4/',
+					img: 'http://www.mapascoletivos.com.br/uploads/images/300_1_1350313222.jpg',
+					url: 'http://www.mapascoletivos.com.br/maps/5318c652cb160eaf6d300ff4/',
 					description: 'Onde adotar um bichinho de estimação (não se esqueça, não compre, adote, tem muitos cães e gatos esperando um dono que os ame)'
 				}
 			]
