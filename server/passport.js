90d00c63500babaec18128946dcd63270f8130a5
Vitor George <vitor.george@gmail.com>
2019-04-24 02:24:51 +0100

    Lint files.

--- server/passport.js
+++ server/passport.js
@@ -1,67 +1,78 @@
-var 
-	mongoose = require('mongoose'),
-	LocalStrategy = require('passport-local').Strategy,
-	BearerStrategy = require('passport-http-bearer').Strategy,
-	User = mongoose.model('User'),
-	AccessToken = mongoose.model('AccessToken');
+var mongoose = require('mongoose');
+var LocalStrategy = require('passport-local').Strategy;
+var BearerStrategy = require('passport-http-bearer').Strategy;
+var User = mongoose.model('User');
+var AccessToken = mongoose.model('AccessToken');
 
 module.exports = function (passport, config) {
+  // serialize sessions
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
 
-	// serialize sessions
-	passport.serializeUser(function(user, done) {
-		done(null, user.id)
-	})
+  passport.deserializeUser(function (id, done) {
+    User.findOne({ _id: id }, function (err, user) {
+      done(err, user);
+    });
+  });
 
-	passport.deserializeUser(function(id, done) {
-		User.findOne({ _id: id }, function (err, user) {
-			done(err, user)
-		})
-	})
+  // use local strategy
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+      },
+      function (req, email, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+          if (err) return done(err);
+          else if (!user) {
+            return done(null, false, {
+              message: 'access_token.local.user_not_registered'
+            });
+          } else if (user.status === 'to_migrate' || !user.hashed_password) {
+            var data = {
+              user: user,
+              callbackUrl: req.app.locals.settings.general.clientUrl + '/login'
+            };
 
-	// use local strategy
-	passport.use(new LocalStrategy({
-			usernameField: 'email',
-			passwordField: 'password',
-			passReqToCallback: true
-		},
-		function(req, email, password, done) {
-			User.findOne({ email: email }, function (err, user) {
-				if (err) 
-					return done(err)
-				else if (!user) 
-					return done(null, false, { message: 'access_token.local.user_not_registered' })
-				else if (user.status == 'to_migrate' || !user.hashed_password) {
+            req.app.locals.mailer.sendEmail(
+              'password_reset',
+              user.email,
+              data,
+              req.i18n,
+              function (err) {
+                if (err) {
+                  return done(err, false, {
+                    message: 'access_token.local.needs_migration.error'
+                  });
+                } else {
+                  return done(null, false, {
+                    message: 'access_token.local.needs_migration.sent'
+                  });
+                }
+              }
+            );
+          } else if (user.hashed_password && !user.authenticate(password)) {
+            return done(null, false, {
+              message: 'access_token.local.invalid_password'
+            });
+          } else return done(null, user);
+        });
+      }
+    )
+  );
 
-					var data = {
-						user: user,
-						callbackUrl: req.app.locals.settings.general.clientUrl + '/login'
-					}
+  passport.use(
+    new BearerStrategy({}, function (token, done) {
+      AccessToken.load({ _id: token }, function (err, token) {
+        if (err) return done(err);
 
-					req.app.locals.mailer.sendEmail('password_reset', user.email, data, req.i18n, function(err) {
-						if (err) 
-		          			return done(err, false, { message: 'access_token.local.needs_migration.error' })
-						else 
-		          			return done(null, false, { message: 'access_token.local.needs_migration.sent' })
-					});
-				}
-				else if (user.hashed_password && !user.authenticate(password))
-          			return done(null, false, { message: 'access_token.local.invalid_password' })
-				else 
-					return done(null, user);
-			})
-		}
-	))
+        if (!token || !token.user) return done(null, false);
 
-	passport.use(new BearerStrategy({}, function(token, done) {
-		AccessToken.load({'_id': token}, function(err, token) {
-			if(err)
-				return done(err);
-
-			if(!token || !token.user)
-				return done(null, false);
-
-			return done(null, token.user);
-		});
-	}));
-
-}
+        return done(null, token.user);
+      });
+    })
+  );
+};
--- server/routes.js
+++ server/routes.js
@@ -1,25 +1,13 @@
-
-/**
- * Module dependencies.
- */
-
-var mongoose = require('mongoose');
-
-/**
- * Controllers dependencies.
- */
-
-var
-	token = require('../app/controllers/token'),
-	accessToken = require('../app/controllers/access_token')
-	maps = require('../app/controllers/maps'),
-	layers = require('../app/controllers/layers'),
-	features = require('../app/controllers/features'),
-	contents = require('../app/controllers/contents'),
-	images = require('../app/controllers/images'),
-	users = require('../app/controllers/users'),
-	admin = require('../app/controllers/admin'),
-	auth = require('./middlewares/authorization');
+const token = require('../app/controllers/token');
+const accessToken = require('../app/controllers/access_token');
+const maps = require('../app/controllers/maps');
+const layers = require('../app/controllers/layers');
+const features = require('../app/controllers/features');
+const contents = require('../app/controllers/contents');
+const images = require('../app/controllers/images');
+const users = require('../app/controllers/users');
+const admin = require('../app/controllers/admin');
+const auth = require('./middlewares/authorization');
 const { isInt } = require('../lib/utils');
 
 /**
@@ -27,8 +15,7 @@ const { isInt } = require('../lib/utils');
  */
 
 module.exports = function (app, passport) {
-
-	var apiPrefix = '/api/v1';
+  var apiPrefix = '/api/v1';
 
   // Sanitize pagination parameters
   app.all('*', function (req, res, next) {
@@ -39,155 +26,209 @@ module.exports = function (app, passport) {
     next();
   });
 
-	/**
-	 * Users routes
-	 **/
-	app.post(apiPrefix + '/forgot_password', users.resetPasswordToken);
+  /**
+   * Users routes
+   **/
+  app.post(apiPrefix + '/forgot_password', users.resetPasswordToken);
 
-	app.post(apiPrefix + '/users', auth.usersCanSignup, users.create);
-	app.put(apiPrefix + '/users', auth.requiresLogin, users.update);
-	app.get(apiPrefix + '/users/:userId', users.show);
+  app.post(apiPrefix + '/users', auth.usersCanSignup, users.create);
+  app.put(apiPrefix + '/users', auth.requiresLogin, users.update);
+  app.get(apiPrefix + '/users/:userId', users.show);
 
-	app.get(apiPrefix + '/user', auth.requiresLogin, users.info);
-	app.get(apiPrefix + '/user/layers', auth.requiresLogin, users.layers);
-	app.get(apiPrefix + '/user/maps', auth.requiresLogin, users.maps);
+  app.get(apiPrefix + '/user', auth.requiresLogin, users.info);
+  app.get(apiPrefix + '/user/layers', auth.requiresLogin, users.layers);
+  app.get(apiPrefix + '/user/maps', auth.requiresLogin, users.maps);
 
-	/**
-	 * Token routes
-	 **/
+  /**
+   * Token routes
+   **/
 
-	// activate account
-	app.get('/confirm_email/:tokenId', token.confirmEmail);
+  // activate account
+  app.get('/confirm_email/:tokenId', token.confirmEmail);
 
-	// accept invitation
-	app.get('/accept_invitation/:tokenId', token.acceptInvitationForm);
-	app.post('/accept_invitation/:tokenId', token.acceptInvitation);
+  // accept invitation
+  app.get('/accept_invitation/:tokenId', token.acceptInvitationForm);
+  app.post('/accept_invitation/:tokenId', token.acceptInvitation);
 
-	// define new passord
-	app.get('/new_password/:tokenId', token.newPasswordForm);
-	app.post('/password_reset/:tokenId', token.newPassword);
+  // define new password
+  app.get('/new_password/:tokenId', token.newPasswordForm);
+  app.post('/password_reset/:tokenId', token.newPassword);
 
-	// change e-mail
-	app.get('/email_change/:tokenId', token.emailChange);
-	app.param('tokenId', token.load);
+  // change e-mail
+  app.get('/email_change/:tokenId', token.emailChange);
+  app.param('tokenId', token.load);
 
+  /**
+   * API ROUTES
+   **/
 
-	/**
-	 * API ROUTES
-	 **/
+  app.get(apiPrefix + '/settings', admin.apiSettings);
 
-	app.get(apiPrefix + '/settings', admin.apiSettings);
+  /**
+   * Access token
+   **/
+  app.post(apiPrefix + '/access_token/local', accessToken.local);
+  app.post(apiPrefix + '/access_token/facebook', accessToken.facebook);
+  app.post(apiPrefix + '/access_token/google', accessToken.google);
+  app.get(apiPrefix + '/access_token/logout', accessToken.logout);
 
-	/**
-	 * Access token
-	 **/
-	app.post(apiPrefix + '/access_token/local', accessToken.local);
-	app.post(apiPrefix + '/access_token/facebook', accessToken.facebook);
-	app.post(apiPrefix + '/access_token/google', accessToken.google);
-	app.get(apiPrefix + '/access_token/logout', accessToken.logout);
+  app.param('userId', users.user);
 
-	app.param('userId', users.user)
+  /**
+   * Feature routes
+   **/
+  app.param('featureId', features.load);
+  // new feature should be associated to a layer
+  app.get(apiPrefix + '/features', features.index);
+  app.get(apiPrefix + '/features/:featureId', features.show);
+  app.post(
+    apiPrefix + '/layers/:layerId/features',
+    [auth.requiresLogin, auth.feature.canCreate],
+    features.create
+  );
+  app.post(
+    apiPrefix + '/layers/:layerId/features/import',
+    [auth.requiresLogin, auth.feature.canCreate],
+    features.import
+  );
+  app.put(
+    apiPrefix + '/features/:featureId',
+    [auth.requiresLogin, auth.feature.canEditOrDelete],
+    features.update
+  );
 
-	/**
-	 * Feature routes
-	 **/
-	app.param('featureId', features.load)
-	// new feature should be associated to a layer
-	app.get(apiPrefix + '/features', features.index);
-	app.get(apiPrefix + '/features/:featureId', features.show);
-	app.post(apiPrefix + '/layers/:layerId/features', [auth.requiresLogin, auth.feature.canCreate], features.create);
-	app.post(apiPrefix + '/layers/:layerId/features/import', [auth.requiresLogin, auth.feature.canCreate], features.import);
-	app.put(apiPrefix + '/features/:featureId', [auth.requiresLogin, auth.feature.canEditOrDelete], features.update);
+  /**
+   * Content routes
+   **/
 
-	/**
-	 * Content routes
-	 **/
+  app.param('contentId', contents.load);
+  // new content should be associated to a layer
+  app.get(apiPrefix + '/contents/:contentId', contents.show);
+  app.get(apiPrefix + '/contents', contents.index);
+  app.post(
+    apiPrefix + '/contents',
+    [auth.requiresLogin, auth.content.canCreate],
+    contents.create
+  );
+  app.put(
+    apiPrefix + '/contents/:contentId',
+    [auth.requiresLogin, auth.content.canEditOrDelete],
+    contents.update
+  );
+  app.del(
+    apiPrefix + '/contents/:contentId',
+    [auth.requiresLogin, auth.content.canEditOrDelete],
+    contents.destroy
+  );
 
-	app.param('contentId', contents.load);
-	// new content should be associated to a layer
-	app.get(apiPrefix + '/contents/:contentId', contents.show);
-	app.get(apiPrefix + '/contents', contents.index);
-	app.post(apiPrefix + '/contents', [auth.requiresLogin, auth.content.canCreate], contents.create);
-	app.put(apiPrefix + '/contents/:contentId', [auth.requiresLogin, auth.content.canEditOrDelete], contents.update);
-	app.del(apiPrefix + '/contents/:contentId', [auth.requiresLogin, auth.content.canEditOrDelete], contents.destroy);
+  /**
+   * Layer routes
+   **/
+  app.param('layerId', layers.load);
+  app.get(apiPrefix + '/layers', auth.loadUser, layers.index);
+  app.post(apiPrefix + '/layers', auth.requiresLogin, layers.create);
+  app.get(apiPrefix + '/layers/:layerId', layers.show);
+  app.del(
+    apiPrefix + '/layers/:layerId',
+    [auth.requiresLogin, auth.layer.requireOwnership],
+    layers.destroy
+  );
+  app.put(
+    apiPrefix + '/layers/:layerId',
+    [auth.requiresLogin, auth.layer.requireOwnership],
+    layers.update
+  );
+  app.put(
+    apiPrefix + '/layers/:layerId/contributors/add',
+    [auth.requiresLogin, auth.layer.requireOwnership],
+    layers.addContributor
+  );
+  app.del(
+    apiPrefix + '/layers/:layerId/contributors/remove',
+    [auth.requiresLogin, auth.layer.requireOwnership],
+    layers.removeContributor
+  );
 
-	/**
-	 * Layer routes
-	 **/
-	app.param('layerId', layers.load);
-	app.get(apiPrefix + '/layers', auth.loadUser, layers.index);
-	app.post(apiPrefix + '/layers', auth.requiresLogin, layers.create);
-	app.get(apiPrefix + '/layers/:layerId', layers.show);
-	app.del(apiPrefix + '/layers/:layerId', [auth.requiresLogin, auth.layer.requireOwnership], layers.destroy);
-	app.put(apiPrefix + '/layers/:layerId', [auth.requiresLogin, auth.layer.requireOwnership], layers.update);
-	app.put(apiPrefix + '/layers/:layerId/contributors/add', [auth.requiresLogin, auth.layer.requireOwnership], layers.addContributor);
-	app.del(apiPrefix + '/layers/:layerId/contributors/remove', [auth.requiresLogin, auth.layer.requireOwnership], layers.removeContributor);
+  /**
+   * Map routes
+   **/
+  app.param('mapId', maps.load);
+  app.get(apiPrefix + '/maps', maps.index);
+  app.post(apiPrefix + '/maps', auth.requiresLogin, maps.create);
+  app.del(apiPrefix + '/maps/:mapId', auth.requiresLogin, maps.destroy);
+  app.put(apiPrefix + '/maps/:mapId', auth.requiresLogin, maps.update);
+  app.get(apiPrefix + '/maps/:mapId', maps.show);
 
+  /**
+   * Images routes
+   **/
+  app.param('imageId', images.load);
+  app.get(apiPrefix + '/images/:imageId', auth.requiresLogin, images.show);
+  app.get('/images', images.showForm);
+  app.post(apiPrefix + '/images', auth.requiresLogin, images.create);
 
-	/**
-	 * Map routes
-	 **/
-	app.param('mapId', maps.load);
-	app.get(apiPrefix + '/maps', maps.index);
-	app.post(apiPrefix + '/maps', auth.requiresLogin, maps.create);
-	app.del(apiPrefix + '/maps/:mapId', auth.requiresLogin, maps.destroy);
-	app.put(apiPrefix + '/maps/:mapId', auth.requiresLogin, maps.update);
-	app.get(apiPrefix + '/maps/:mapId', maps.show);
+  /**
+   * Association routes
+   **/
 
+  // layer x feature
+  app.put(
+    apiPrefix + '/layers/:layerId/features/:featureId',
+    [auth.requiresLogin, auth.feature.canEditOrDelete],
+    layers.addFeature
+  );
+  app.del(
+    apiPrefix + '/layers/:layerId/features/:featureId',
+    [auth.requiresLogin, auth.feature.canEditOrDelete],
+    layers.removeFeature
+  );
 
-	/**
-	 * Images routes
-	 **/
-	app.param('imageId', images.load);
-	app.get(apiPrefix + '/images/:imageId', auth.requiresLogin, images.show);
-	app.get('/images', images.showForm);
-	app.post(apiPrefix + '/images', auth.requiresLogin, images.create);
+  // feature x content
+  app.put(
+    apiPrefix + '/features/:featureId/contents/:contentId',
+    auth.requiresLogin,
+    features.addContent
+  );
+  app.del(
+    apiPrefix + '/features/:featureId/contents/:contentId',
+    auth.requiresLogin,
+    features.removeContent
+  );
 
-	/**
-	 * Association routes
-	 **/
+  /**
+   * ADMIN ROUTES
+   */
 
-	// layer x feature
-	app.put(apiPrefix + '/layers/:layerId/features/:featureId', [auth.requiresLogin, auth.feature.canEditOrDelete], layers.addFeature);
-	app.del(apiPrefix + '/layers/:layerId/features/:featureId', [auth.requiresLogin, auth.feature.canEditOrDelete], layers.removeFeature);
+  app.get('/admin/first_admin', admin.firstAdminForm);
+  app.post('/admin/first_admin', admin.firstAdmin);
 
-	// feature x content
-	app.put(apiPrefix + '/features/:featureId/contents/:contentId', auth.requiresLogin, features.addContent);
-	app.del(apiPrefix + '/features/:featureId/contents/:contentId', auth.requiresLogin, features.removeContent);
+  app.get('/admin/login', admin.login);
+  app.get('/admin/logout', admin.logout);
+  app.post(
+    '/admin/session',
+    passport.authenticate('local', {
+      failureRedirect: '/admin/login',
+      failureFlash: 'Invalid email or password.'
+    }),
+    admin.session
+  );
 
-	/**
-	 * ADMIN ROUTES
-	 */
+  app.get('/admin', auth.isAdmin, admin.index);
 
-	app.get('/admin/first_admin', admin.firstAdminForm);
-	app.post('/admin/first_admin', admin.firstAdmin);
+  // Global settings
+  app.get('/admin/settings', auth.isAdmin, admin.settings);
+  app.post('/admin/settings', auth.isAdmin, admin.update);
+  app.get('/admin/settings/mail', auth.isAdmin, admin.mailForm);
+  app.post('/admin/settings/mail', auth.isAdmin, admin.mail);
 
-	app.get('/admin/login', admin.login);
-	app.get('/admin/logout', admin.logout);
-	app.post('/admin/session',
-			passport.authenticate('local', {
-				failureRedirect: '/admin/login',
-				failureFlash: 'Invalid email or password.'
-			}), admin.session);
+  // User administration
+  app.get('/admin/users', auth.isAdmin, admin.users);
+  app.get('/admin/users/invite', auth.isAdmin, admin.inviteForm);
+  app.post('/admin/users/invite', auth.isAdmin, admin.invite);
+  app.get('/admin/users/roles', auth.isAdmin, admin.roles);
+  app.post('/admin/users/role', auth.isAdmin, admin.changeRole);
 
-	app.get('/admin', auth.isAdmin, admin.index);
-
-	// Global settings
-	app.get('/admin/settings', auth.isAdmin, admin.settings);
-	app.post('/admin/settings', auth.isAdmin, admin.update);
-	app.get('/admin/settings/mail', auth.isAdmin, admin.mailForm);
-	app.post('/admin/settings/mail', auth.isAdmin, admin.mail);
-
-	// User administration
-	app.get('/admin/users', auth.isAdmin, admin.users);
-	app.get('/admin/users/invite', auth.isAdmin, admin.inviteForm);
-	app.post('/admin/users/invite', auth.isAdmin, admin.invite);
-	app.get('/admin/users/roles', auth.isAdmin, admin.roles);
-	app.post('/admin/users/role', auth.isAdmin, admin.changeRole);
-
-	app.get('/', function(req, res){
-		res.redirect('/admin');
-	});
-
-}
-
+  app.get('/', function (req, res) {
+    res.redirect('/admin');
+  });
+};
