7fe8e3088425d432e19083f9c59f17c9d27fa2cd
Vitor George <vitor.george@gmail.com>
2019-06-09 12:20:36 +0100

    At test suite, load "imagesPath" from config file.

--- test/test--index.js
+++ test/test--index.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const config = require('config');
+const { join } = require('path');
 const server = require('../web');
 const mongoose = require('mongoose');
 
 // Export globals
 global.server = server;
 global.apiUrl = pathname => `http://localhost:3000${pathname}`;
-global.imagesPath = path.join(__dirname, '..', 'tmp', 'test-images');
+global.imagesPath = join(__dirname, '..', ...config.get('imagesPath'));
 
 describe('Mapas Coletivos', function () {
   before(done => {
